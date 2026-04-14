591981b218acd2df1ec4519025e50822ba74c2a7
kusnarevakurshab-maker <kusnarevakurshab@gmail.com>
2026-04-14 20:24:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,6 +129,9 @@ def main():
     app.run_polling()
 
 
-if __name__ == "__main__":
-    main()
+# ---------------- RUN ----------------
+import asyncio
 
+if __name__ == "__main__":
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    main()
